7898fdf754ad841e9f60b21dd7266115c1049d72
larissamy <74251920+larissamy@users.noreply.github.com>
2026-04-11 18:18:44 -0300

    feat: add OpenAPI configuration with JWT security scheme for sales API

--- src/main/java/com/fiap/carsales/presentation/controllers/SalesController.java
+++ src/main/java/com/fiap/carsales/presentation/controllers/SalesController.java
@@ -3,6 +3,7 @@ package com.fiap.carsales.presentation.controllers;
 import com.fiap.carsales.application.dto.request.RegisterSaleRequest;
 import com.fiap.carsales.application.dto.response.SaleResponse;
 import com.fiap.carsales.application.interfaces.SaleServicePort;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/sales")
+@SecurityRequirement(name = "bearerAuth")
 public class SalesController {
 
     private final SaleServicePort service;
